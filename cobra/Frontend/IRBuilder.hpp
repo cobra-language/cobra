88eb0e6a7e1316df5fbbe38bf2396ee57e92d217
Cao Roy <roy.cao@shopee.com>
2024-01-03 10:14:02 +0800

    Add IR

---
+++ cobra/Frontend/IRBuilder.hpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) the Cobra project authors.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef IRBuilder_hpp
+#define IRBuilder_hpp
+
+#include <stdio.h>
+
+#endif /* IRBuilder_hpp */

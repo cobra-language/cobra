2ea3698832bf3ffa7f2bc4dcc0fc44eb27c9cfee
Cao Roy <roy.cao@shopee.com>
2024-01-18 19:11:03 +0800

    Add Scope

--- include/cobra/IR/Scope.h
+++ include/cobra/IR/Scope.h
@@ -9,5 +9,32 @@
 #define Scope_hpp
 
 #include <stdio.h>
+#include <vector>
+#include "cobra/IR/IR.h"
 
+namespace cobra {
+
+class Scope {
+  using VariableListType = std::vector<Variable *>;
+  
+  Scope(const Scope &) = delete;
+  Scope &operator=(const Scope &) = delete;
+  
+  explicit Scope(Scope *p) : parent(p) {}
+
+  ~Scope();
+  
+public:
+  
+  Scope *getParent() const {
+    return parent;
+  }
+  
+  
+private:
+  Scope *parent{};
+  
+};
+
+}
 #endif /* Scope_hpp */

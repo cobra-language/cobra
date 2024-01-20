dc645b67176ab54dd560c5fdb3b993df205aeb50
Cao Roy <roy.cao@shopee.com>
2024-01-20 17:32:24 +0800

    Add SemanticChecker File

---
+++ lib/AST/SemanticChecker.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) the Cobra project authors.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#ifndef SemanticChecker_hpp
+#define SemanticChecker_hpp
+
+#include <stdio.h>
+#include "cobra/AST/ASTVisitor.h"
+#include "cobra/AST/Tree.h"
+
+namespace cobra {
+
+class SemanticChecker {
+  
+  
+  
+};
+
+
+}
+
+#endif /* SemanticChecker_h */
